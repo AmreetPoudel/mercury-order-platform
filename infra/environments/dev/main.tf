c3255ceb7b39f9ebb53c0b6a59663f06a5ab2fa2
amrit poudel <aamreet12345@gmail.com>
2026-05-17 11:31:34 +0545

    dev vpc var

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -1,5 +1,9 @@
 module "vpc" {
   source = "../../modules/vpc"
+
+  vpc_cidr           = "10.0.0.0/16"
+  public_subnet_cidr = "10.0.1.0/24"
+  availability_zone  = "ap-south-1a"
 }
 
 module "sg" {
