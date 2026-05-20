b9459539f33da1d354561002247445696526efdc
amrit poudel <aamreet12345@gmail.com>
2026-05-20 16:23:51 +0545

    user-data updated

--- infra/modules/ec2/user_data.sh
+++ infra/modules/ec2/user_data.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-sudo apt update -y
+apt update -y
 
-sudo apt install -y \
+apt install -y \
     docker.io \
     docker-compose \
     git
 
-sudo systemctl enable docker
-sudo systemctl start docker
+systemctl enable docker
+systemctl start docker
 
 usermod -aG docker ubuntu
 
 mkdir -p /home/ubuntu/mercury-order-platform
 
-sudo chown ubuntu:ubuntu /home/ubuntu/mercury-order-platform
+chown ubuntu:ubuntu /home/ubuntu/mercury-order-platform
