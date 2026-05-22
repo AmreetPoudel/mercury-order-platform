67c76ffde9ebb7e60bbb7ba9bf683aa9dcf8220d
amrit poudel <aamreet12345@gmail.com>
2026-05-22 16:49:24 +0545

    userdata modify

--- infra/modules/ec2/user_data.sh
+++ infra/modules/ec2/user_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -eux
 
 apt update -y
 
@@ -8,7 +8,14 @@ apt install -y \
     docker.io \
     docker-compose-v2 \
     git \
-    awscli
+    unzip \
+    curl
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o awscliv2.zip
+
+unzip awscliv2.zip
+
+./aws/install
 
 systemctl enable docker
 systemctl start docker
@@ -17,4 +24,4 @@ usermod -aG docker ubuntu
 
 mkdir -p /home/ubuntu/mercury-order-platform
 
-chown ubuntu:ubuntu /home/ubuntu/mercury-order-platform
+chown -R ubuntu:ubuntu /home/ubuntu/mercury-order-platform
