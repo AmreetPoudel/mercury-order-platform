12e7d6c5492920dc18092e35bcaffa2bd1e71d38
amrit poudel <aamreet12345@gmail.com>
2026-05-22 15:55:30 +0545

    error

--- infra/modules/ec2/main.tf
+++ infra/modules/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2" {
   ami           = "ami-0388e3ada3d9812da" # 
   instance_type = "t3.micro"
   subnet_id = var.subnet_id
-  vpc_security_group_ids=var.vpc_security_group_id
+  vpc_security_group_ids = var.vpc_security_group_ids
   iam_instance_profile = var.instance_profile_name
   associate_public_ip_address = true
   key_name = "mercury-key"
